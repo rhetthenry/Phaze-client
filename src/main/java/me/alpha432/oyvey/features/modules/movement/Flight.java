294d0f650f51c63ae6c28247a6530615f966adba
Rhettcactus <161527742+rhetthenry@users.noreply.github.com>
2025-10-28 18:12:11 -0500

    Create Flight.java

---
+++ src/main/java/me/alpha432/oyvey/features/modules/movement/Flight.java
@@ -0,0 +1,13 @@
+package me.alpha432.oyvey.features.modules.movement;
+
+import me.alpha432.oyvey.features.modules.Module;
+import me.alpha432.oyvey.features.settings.Setting;
+import net.minecraft.entity.attribute.EntityAttributes;
+
+public class extends Module {
+
+  public fly() {
+    super("Fly", "Flight!!", Catagory.MOVMENT, true, false, fasle);
+  }
+}
+
